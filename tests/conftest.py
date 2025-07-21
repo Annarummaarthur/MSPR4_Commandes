bd16df122072c3b28c56109dcfc97a7d80e095f0
Beyou <159903460+Beyou-Pro@users.noreply.github.com>
2025-07-21 14:42:05 +0200

    [ADD] ci

---
+++ tests/conftest.py
@@ -0,0 +1,42 @@
+import pytest
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.db import Base, get_db
+from app.main import app
+from starlette.testclient import TestClient
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+@pytest.fixture(scope="function")
+def db_engine():
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    Base.metadata.create_all(bind=engine)
+    yield engine
+    Base.metadata.drop_all(bind=engine)
+
+
+@pytest.fixture(scope="function")
+def db_session(db_engine):
+    TestingSessionLocal = sessionmaker(
+        autocommit=False, autoflush=False, bind=db_engine
+    )
+    session = TestingSessionLocal()
+    yield session
+    session.close()
+
+
+@pytest.fixture(scope="function")
+def auth_headers():
+    return {"Authorization": "Bearer mspr4_commandes_api_token_secure_2025"}
+
+
+@pytest.fixture(scope="function")
+def client(db_session):
+    def override_get_db():
+        yield db_session
+
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+    app.dependency_overrides.clear()
